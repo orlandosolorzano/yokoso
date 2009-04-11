5c4d45858c8c0317e4f1bacc9f470e507bf82081
dong128 <dong128@d48f3c38-1e47-11de-b69a-45dc1c5e4070>
2009-04-11 08:44:44 +0000

    Developed by CodeBlocks!

---
+++ trunk/yokoso-codeblocks/globaldef.h
@@ -0,0 +1,16 @@
+#ifndef TYPEDEF_H_INCLUDED
+#define TYPEDEF_H_INCLUDED
+#endif // TYPEDEF_H_INCLUDED
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <string>
+
+using namespace std;
+
+typedef map<string, int> SymbolMap;
+typedef pair<int, int> Position;
+typedef set<string> SymbolSet;
+typedef vector<string> Production;
